ac027ed6dab7ccf0b986f9178d779b9fceebe8d4
carloseur24 <carloseur24@gmail.com>
2025-01-11 18:54:20 -0400

    add: sample main.go

---
+++ github-tracker-app/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func postHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Received Post Request")
+
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error reading Request")
+		return
+	}
+
+	fmt.Println("Received Body: ", string(body))
+}
+
+func main(){
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello",postHandler).Methods("POST")
+
+	fmt.Println("Server is running on port 8080")
+	err := http.ListenAndServe(":8080", router)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github-tracker
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
