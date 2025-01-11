35519562748687eddcac007b4e69fcea3c0d6e2e
carloseur24 <carloseur24@gmail.com>
2025-01-11 19:02:40 -0400

    fix: update server running message to include full URL

--- github-tracker-app/main.go
+++ github-tracker-app/main.go
@@ -27,7 +27,7 @@ func main(){
 
 	router.HandleFunc("/hello",postHandler).Methods("POST")
 
-	fmt.Println("Server esta corrindo no porto 8080")
+	fmt.Println("Server esta corrindo no porto http://localhost:8080")
 	err := http.ListenAndServe(":8080", router)
 
 	if err != nil {
